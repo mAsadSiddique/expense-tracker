d0e81a617b01d31f52fe46ec3f24a3eb54ffecbd
mAsadSiddique <asadsiddique.a@gmail.com>
2020-10-30 19:51:36 +0500

    Created Header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import TotalExpense from './Components/TotalExpense/TotalExpense';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TotalExpense />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TotalExpense/TotalExpense.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './TotalExpense.css'
+
+function TotalExpense() {
+    return (
+        <div className="class">
+            <span>
+                Total Amount
+            </span>
+            <span>$0.00</span>
+        </div>
+    )
+}
+
+export default TotalExpense
